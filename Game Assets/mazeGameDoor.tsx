26d2c89c16c55631d8fbbd5c1f6db0ef501ddbd5
EvanWeldon <WeldonEvanT@gmail.com>
2022-12-01 20:40:35 -0800

    added game assets files and changed scriptsjs to indexjs

---
+++ Game Assets/mazeGameDoor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="mazeGameDoor" tilewidth="92" tileheight="112" tilecount="1" columns="1">
+ <image source="tiled/assets/door.png" width="92" height="112"/>
+</tileset>
---
+++ Game Assets/mazeGameTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="SDSU Milestone Browser Game" tilewidth="32" tileheight="32" tilecount="988" columns="38">
+ <image source="tiled/assets/Terrain (64x64).png" width="1216" height="832"/>
+</tileset>
