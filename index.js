1511a773b1554d48e4dc6ed4625ebeb52ddb5523
EvanWeldon <WeldonEvanT@gmail.com>
2022-12-01 21:33:21 -0800

    fixed the huge img issue by passing in more arguments to my arrow function

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d')
 
-canvas.width = 2000
-canvas.height = 2000
+canvas.width = 1280
+canvas.height = 720
 
 // creating a const for the level one map and importing the source
 const map1 = new Image()
@@ -11,5 +11,5 @@ map1.src = './img/map1.png'
 console.log(map1)
 
 map1.onload = () => {
-    context.drawImage(map1, 0, 0)
+    context.drawImage(map1, 0, 0, canvas.width, canvas.height)
 }
