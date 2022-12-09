8d6a0d0a2055714a533f5de35fda32e1d6bef132
Evan Weldon <111112873+EvanWeldon@users.noreply.github.com>
2022-12-08 16:06:22 -0800

    Delete sdsu mapset.tsx

--- gameAssets/tiled/maps/sdsu mapset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="sdsu mapset" tilewidth="64" tileheight="64" tilecount="247" columns="19">
- <image source="../assets/Terrain (64x64).png" width="1216" height="832"/>
-</tileset>
