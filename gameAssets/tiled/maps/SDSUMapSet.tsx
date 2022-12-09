8eff48ca7fdb8e455ae67f55b61bc096252c689e
Evan Weldon <111112873+EvanWeldon@users.noreply.github.com>
2022-12-08 16:05:53 -0800

    Delete SDSUMapSet.tsx

--- gameAssets/tiled/maps/SDSUMapSet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="SDSUMapSet" tilewidth="16" tileheight="16" tilecount="3952" columns="76">
- <image source="../assets/Terrain (64x64).png" width="1216" height="832"/>
-</tileset>
