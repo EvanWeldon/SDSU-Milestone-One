f6d431770f17c28b4977e974733c7d5a6211488c
Evan Weldon <111112873+EvanWeldon@users.noreply.github.com>
2022-12-08 16:06:03 -0800

    Delete door.tsx

--- gameAssets/tiled/maps/door.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="door" tilewidth="92" tileheight="112" tilecount="1" columns="1">
- <image source="../assets/door.png" width="92" height="112"/>
-</tileset>
