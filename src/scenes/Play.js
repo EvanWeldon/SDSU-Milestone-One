ec296adfc65c079fbcf7e851d0efa7c02e384521
EvanWeldon <WeldonEvanT@gmail.com>
2022-12-10 00:02:37 -0800

    player now collides with platform layer

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -9,29 +9,37 @@ class Play extends Phaser.Scene {
 
     create() {
     const map = this.createMap();
-    this.createLayers(map);
-    this.createPlayer();
+    const layers = this.createLayers(map);
+
+    const player = this.createPlayer();
+
+    this.physics.add.collider(player, layers.platform);
     }
 
     createMap() {
         const map = this.make.tilemap({key:'map'});
-        const tileset1= map.addTilesetImage('background', 'ts-background');
-        const tileset2= map.addTilesetImage('terrain', 'ts-platform');
+        map.addTilesetImage('background', 'ts-background');
+        map.addTilesetImage('terrain', 'ts-platform');
         return map;
     }
 
     createLayers(map) {
         const tileset1 = map.getTileset('background');
         const tileset2 = map.getTileset('terrain');
-        map.createStaticLayer('background', tileset1 );
-        map.createStaticLayer('platform', tileset2 );
-        return {tileset1, tileset2}
+        const background = map.createStaticLayer('background', tileset1 );
+        const platform = map.createStaticLayer('platform', tileset2 );
+
+        platform.setCollisionByExclusion(-1, true);
+
+        
+        return {background, platform};
     }
 
     createPlayer() {
         const player = this.physics.add.sprite(100, 250, 'player')
         player.body.setGravityY(500);
         player.setCollideWorldBounds(true);
+        return player;
     }
 }
 
