10d5dbcf236e618feba67b9545797e9f95956b18
EvanWeldon <WeldonEvanT@gmail.com>
2022-12-09 23:38:41 -0800

    refactored code a bit with createMap and createLayers createPlayer

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,18 +8,30 @@ class Play extends Phaser.Scene {
     }
 
     create() {
-    const map = this.make.tilemap({key:'map'});
-    const tileset1= map.addTilesetImage('background', 'ts-background');
-    const tileset2= map.addTilesetImage('terrain', 'ts-platform');
-
-    map.createStaticLayer('background', tileset1 );
-    map.createStaticLayer('platform', tileset2 );
+    const map = this.createMap();
+    this.createLayers(map);
     this.createPlayer();
+    }
+
+    createMap() {
+        const map = this.make.tilemap({key:'map'});
+        const tileset1= map.addTilesetImage('background', 'ts-background');
+        const tileset2= map.addTilesetImage('terrain', 'ts-platform');
+        return map;
+    }
+
+    createLayers(map) {
+        const tileset1 = map.getTileset('background');
+        const tileset2 = map.getTileset('terrain');
+        map.createStaticLayer('background', tileset1 );
+        map.createStaticLayer('platform', tileset2 );
+        return {tileset1, tileset2}
     }
 
     createPlayer() {
         const player = this.physics.add.sprite(100, 250, 'player')
-        player.body.setGravity(500);
+        player.body.setGravityY(500);
+        player.setCollideWorldBounds(true);
     }
 }
 
